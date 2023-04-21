5e71023262e804045e3d24525bf92bb0791ca7d9
LebedevAndrey <LebedevAndrey2010@mail.ru>
2023-04-21 20:42:47 +0300

    Test v1

---
+++ python_trening/test_2strig.py
@@ -0,0 +1,15 @@
+s= "Это строка"
+print(s + '\n')
+g = ''' Многострочная
+страка'''
+print(g+'\n')
+print('stroki' + 'mozhno' + 'skladyvat')
+
+a
+print('Avto' * 3)
+
+print(len('test'))
+
+a = 'Avto'
+print(a[0]) # A
+print(a[-2]) # t
