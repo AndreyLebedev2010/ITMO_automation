fa2335b7cfb05af575fb7b2caf72d8afacc8572f
LebedevAndrey <LebedevAndrey2010@mail.ru>
2023-04-21 21:32:24 +0300

    Test v3

---
+++ python_trening/task_8_elif.py
@@ -0,0 +1,12 @@
+num_float = 3.4
+
+# Также попробуйте варианты
+# num_float = 0
+# num_float = -4.5
+
+if num_float > 0:
+    print("+ chislo")
+elif num_float == 0:
+    print("0")
+else:
+    print("chislo -")
---
+++ python_trening/task_8_if_elif_else.py
@@ -0,0 +1,17 @@
+num = 3
+
+if num >= 0:
+    print('Число больше либо равно 0')
+else:
+    print('Число отрицательное')
+
+str_1 = "test123456"
+str_2 = "test12345"
+
+#str_2 содержит в себе str_1 ?
+if str_1 in str_2:
+    print("DA")
+
+else:
+    print("NET")
+
